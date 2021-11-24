852d0e992089e94abe0f27c619e088da5d045a34
Aditya Patel <adityagpatel59@gmail.com>
2021-11-24 08:53:43 +0530

    Add project files.

---
+++ BACKEND_HTML_DOT_NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BACKEND_HTML_DOT_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
