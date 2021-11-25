0a636328541c392dac81a341185ae7e286306ed6
Jahnvi Patel <jspatel19264@gmail.com>
2021-11-25 20:33:20 +0530

    About College Controller Added

---
+++ BACKEND_HTML_DOT_NET/Controllers/About_CollegeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACKEND_HTML_DOT_NET.Controllers
+{
+    public class About_CollegeController : Controller
+    {
+        public IActionResult About_add()
+        {
+            return View();
+        }
+
+        public IActionResult About_view()
+        {
+            return View();
+        }
+    }
+}
