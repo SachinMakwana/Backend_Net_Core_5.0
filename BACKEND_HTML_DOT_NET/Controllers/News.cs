c0cc35d719e71710e446e9e5cf1594a509afe37b
A3939 <adityagpatel59@gmail.com>
2022-07-14 23:21:02 +0530

    adding news

--- BACKEND_HTML_DOT_NET/Controllers/News.cs
+++ BACKEND_HTML_DOT_NET/Controllers/News.cs
@@ -5,15 +5,40 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BACKEND_HTML_DOT_NET.Models;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace BACKEND_HTML_DOT_NET.Controllers
 {
     public class News : Controller
     {
+        private string apiBaseUrl = "https://localhost:44374/api/";
+
         public IActionResult NewsView()
         {
-            IEnumerable<NewsVM> news = null;
+            
 
+            IList<NewsVM> news = new List<NewsVM>();
+
+             using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(news), Encoding.UTF8, "application/json");
+                string endpoint = apiBaseUrl + "GetAllNewsDetails";
+                using (var Response = await client.GetAsync(endpoint))
+                {
+                    if (Response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        TempData["Profile"] = JsonConvert.SerializeObject(news);
+                        return RedirectToAction("Profile");
+                    }
+                    else
+                    {
+                        ModelState.Clear();
+                        ModelState.AddModelError(string.Empty, "Username or Password is Incorrect");
+                        return View();
+                    }
+                }
+            }
 
             return View(news);
         }
