5ef5a70e44bc5020238ea8f69d1d12a51e81bbb5
krish-ktm <58637118+krish-ktm@users.noreply.github.com>
2022-07-18 16:47:29 +0530

    Update Images.cs

--- BACKEND_HTML_DOT_NET/Controllers/Images.cs
+++ BACKEND_HTML_DOT_NET/Controllers/Images.cs
@@ -1,27 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_DOT_NET_API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BACKEND_HTML_DOT_NET.Controllers
 {
     public class Images : Controller
     {
-        public IActionResult ImagesView()
+
+        private string apiBaseUrl = "https://localhost:44374/api";
+        HttpClient hc = new HttpClient();
+        private List<GalleryVM> galleryListVM = new List<GalleryVM>();
+        public IActionResult ImagesView(int id=0)
         {
-            return View();
+            GalleryVM galleryVM = new GalleryVM();
+            galleryVM = galleryListVM.Where(m => m.Id == id).FirstOrDefault();
+
+            return View(galleryVM);
         }
         public IActionResult ImagesAdd()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult ImagesAdd(FromBodyAttribute fromBodyAttribute)
-        {
-
-            return View();
-        }
     }
 }
