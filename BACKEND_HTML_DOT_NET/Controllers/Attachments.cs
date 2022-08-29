00d5200272449347407604513e7beb6bf029d8a5
krish-ktm <58637118+krish-ktm@users.noreply.github.com>
2022-08-29 23:50:08 +0530

    Attachments Added Working

--- BACKEND_HTML_DOT_NET/Controllers/Attachments.cs
+++ BACKEND_HTML_DOT_NET/Controllers/Attachments.cs
@@ -1,24 +1,218 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BACKEND_HTML_DOT_NET.Models;
+using GECP_DOT_NET_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BACKEND_HTML_DOT_NET.Controllers
 {
     public class Attachments : Controller
     {
+        private string apiBaseUrl = "https://localhost:44374/api";
+        HttpClient hc = new HttpClient();
+        private static List<AttachmentVM> attachmentVMList = new List<AttachmentVM>();
+        RestClient client;
+
+        public Attachments()
+        {
+            client = new RestClient(apiBaseUrl);
+        }
+
         public IActionResult AttachmentsAdd()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AttachmentsAdd(IFormCollection collection)
+        {
+            try
+            {
+                AttachmentVM attachmentVM = new AttachmentVM();
+                await TryUpdateModelAsync<AttachmentVM>(attachmentVM);
+                attachmentVM.CreatedDate = DateTime.Now;
+                attachmentVM.UpdatedDate = DateTime.Now;
+                var request = new RestRequest("/AddAttachmentDetail", Method.Post);
+                //add files to request
+                foreach (var file in collection.Files)
+                {
+                    var memorystream = new MemoryStream();
+                    file.CopyTo(memorystream);
+                    var bytes = memorystream.ToArray();
+                    request.AddFile(file.Name.ToString(), bytes, file.FileName.ToString());
+                }
+
+                //iterate and add model to request as parameter
+                PropertyInfo[] properties = typeof(AttachmentVM).GetProperties();
+                foreach (PropertyInfo property in properties)
+                {
+                    var value = property.GetValue(attachmentVM);
+                    request.AddParameter(property.Name.ToString(), value == null ? "" : value.ToString());
+                }
+
+                var response = client.Execute(request);
+                //use response.content --> this will directly give the parsed result.
+                ServiceResponse<bool> serviceResponse = JsonConvert.DeserializeObject<ServiceResponse<bool>>(response.Content);
+                return Json(serviceResponse);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status_code = "000", message = ex.Message.ToString() });
+            }
+        }
+
         public IActionResult AttachmentsView()
         {
-            return View();
+            var restRequest = new RestRequest("/GetAllAttachmentDetails", Method.Get);
+            restRequest.AddHeader("Accept", "application/json");
+            restRequest.RequestFormat = DataFormat.Json;
+
+            RestResponse response = client.Execute(restRequest);
+
+            var content = response.Content;
+            if (content != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<AttachmentVM>>>(content);
+                attachmentVMList = user.data;
+                foreach (var data in attachmentVMList = user.data)
+                {
+                    data.Attachment1 = "https://localhost:44374/" + data.Attachment1;
+                }
+            }
+            return View(attachmentVMList);
+
         }
-        public IActionResult AttachmentsEdit()
+
+
+
+        public IActionResult AttachmentsEdit(long id = 0)
         {
-            return View();
+            AttachmentVM attachmentVM = new AttachmentVM();
+            try
+            {
+
+                attachmentVM = attachmentVMList.Where(m => m.Id == id).FirstOrDefault();
+
+                var restRequest = new RestRequest("/GetAllAttachmentDetails", Method.Get);
+                restRequest.AddHeader("Accept", "application/json");
+                restRequest.RequestFormat = DataFormat.Json;
+                RestResponse response = client.Execute(restRequest);
+
+                var content = response.Content;
+                if (content != null)
+                {
+                    var user = JsonConvert.DeserializeObject<ServiceResponse<List<AttachmentVM>>>(content);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View(attachmentVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AttachmentsEdit(IFormCollection collection)
+        {
+            try
+            {
+                AttachmentVM attachmentVM = new AttachmentVM();
+                await TryUpdateModelAsync<AttachmentVM>(attachmentVM);
+                attachmentVM.UpdatedDate = DateTime.Now;
+                RestRequest request = new RestRequest("/UpdateAttachmentDetail", Method.Put);
+
+                if (collection.Files.Count() > 0)
+                {
+                    //add files to request
+                    foreach (var file in collection.Files)
+                    {
+                        var memorystream = new MemoryStream();
+                        file.CopyTo(memorystream);
+                        var bytes = memorystream.ToArray();
+                        request.AddFile(file.Name.ToString(), bytes, file.FileName.ToString());
+                    }
+                }
+                else
+                {
+                    byte[] data = new byte[0];
+                    request.AddFile("image", data, "noimage");
+                }
+
+
+                //iterate and add model to request as parameter
+                PropertyInfo[] properties = typeof(AttachmentVM).GetProperties();
+                foreach (PropertyInfo property in properties)
+                {
+                    if (property.Name.ToString() != "FacultySelectList")
+                    {
+                        var value = property.GetValue(attachmentVM);
+                        request.AddParameter(property.Name.ToString(), value == null ? "" : value.ToString());
+                    }
+                }
+
+                var response = client.Execute(request);
+                //use response.content --> this will directly give the parsed result.
+                ServiceResponse<bool> serviceResponse = JsonConvert.DeserializeObject<ServiceResponse<bool>>(response.Content);
+                return Json(serviceResponse);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status_code = "000", message = ex.Message.ToString() });
+            }
+
+        }
+
+      
+
+        [HttpPost]
+        public async Task<IActionResult> AttachmentsDelete(long id)
+        {
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(id.ToString()))
+                {
+                    return Json(new { message = "Invalid Record." });
+                }
+                var updateItem = attachmentVMList.Where(m => m.Id == id).FirstOrDefault();
+                updateItem.IsDeleted = true;
+                updateItem.UpdatedDate = DateTime.Now;
+                using (var client = new HttpClient())
+                {
+                    var uri = new Uri(apiBaseUrl + "/DeleteAttachmentDetail");
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(updateItem), Encoding.UTF8, "application/json");
+
+                    using (var response = client.PutAsync(uri, content))
+                    {
+                        response.Wait();
+                        var results = response.Result;
+                        var jsonString = await results.Content.ReadAsStringAsync();
+
+                        var res = JsonConvert.DeserializeObject<ServiceResponse<bool>>(jsonString);
+                        if (results.IsSuccessStatusCode)
+                        {
+                            return Json(res);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { message = ex.Message.ToString() });
+            }
+            return Json(new { message = "something went wrong." });
         }
     }
 }
---
+++ BACKEND_HTML_DOT_NET/Models/AttachmentVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GECP_DOT_NET_API.Models
+{
+    public class AttachmentVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Attachment1 { get; set; }
+        public int PageId { get; set; }
+        public bool? IsVisible { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedDateInt { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedDateInt { get; set; }
+    }
+}
