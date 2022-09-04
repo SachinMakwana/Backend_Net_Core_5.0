5001ce7a1af52c0c5a46f0606bcaf056959c76e2
ADITYA PATEL <adityagpatel59@gmail.com>
2022-09-04 13:42:00 +0530

    women cell view created

--- BACKEND_HTML_DOT_NET/Controllers/WomenCell.cs
+++ BACKEND_HTML_DOT_NET/Controllers/WomenCell.cs
@@ -1,29 +1,116 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BACKEND_HTML_DOT_NET.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BACKEND_HTML_DOT_NET.Controllers
 {
     public class WomenCell : Controller
     {
-        public IActionResult WomenCellAdd()
+        private string apiBaseUrl = "https://localhost:44374/api";
+        HttpClient hc = new HttpClient();
+        private static List<CommitteeVM> committeList = new List<CommitteeVM>();
+        RestClient client;
+
+        public WomenCell()
         {
-            return View();
+            client = new RestClient(apiBaseUrl);
         }
+
+        public IActionResult WomenCellEdit()
+        {
+            CommitteeVM committeeVM = new CommitteeVM();
+            try
+            {
+                committeeVM = committeList.Where(m => m.Id == 1).FirstOrDefault();
+
+            }catch(Exception ex)
+            {
+
+            }
+            return View(committeeVM);
+        }
+
+        [HttpPost]
+        public IActionResult WomenCellEdit([FromForm] CommitteeVM womencellVM, [Optional] IFormCollection collection)
+        {
+            try
+            {
+                womencellVM.UpdatedDate = DateTime.Now;
+                RestRequest request = new RestRequest("UpdateCommitteeDetail", Method.Put);
+
+                if(collection.Files.Count() > 0)
+                {
+                    foreach (var file in collection.Files)
+                    {
+                        var memorystream = new MemoryStream();
+                        file.CopyTo(memorystream);
+                        var bytes = memorystream.ToArray();
+                        request.AddFile(file.Name.ToString(), bytes, file.FileName.ToString());
+                    }
+                }
+                else {
+                    byte[] data = new byte[0];
+                    request.AddFile("image", data, "NoImage");
+                }
+
+                PropertyInfo[] properties = typeof(CommitteeVM).GetProperties();
+                foreach (PropertyInfo property in properties)
+                {
+                    if (property.Name.ToString() != "FacultySelectList")
+                    {
+                        var value = property.GetValue(womencellVM);
+                        request.AddParameter(property.Name.ToString(), value == null ? "" : value.ToString());
+                    }
+                }
+
+                var response = client.Execute(request);
+                ServiceResponse<bool> serviceResponse = JsonConvert.DeserializeObject<ServiceResponse<bool>>(response.Content);
+                return Json(serviceResponse);
+
+
+            }
+            catch(Exception ex)
+            {
+                return Json(new { status_code = "000", message = ex.Message.ToString() });
+            }
+        }
+
+       // public IActionResult WomenCellView()
+       // {
+       //     return View();
+        //}
+
+        
         public IActionResult WomenCellView()
         {
-            return View();
+            var restClient = new RestClient(apiBaseUrl);
+            var restRequest = new RestRequest("/GetAllCommitteeDetails", Method.Get);
+            restRequest.AddHeader("Accept", "application/json");
+            restRequest.RequestFormat = DataFormat.Json;
+
+            RestResponse response = restClient.Execute(restRequest);
+
+            var content = response.Content;
+
+            var user = JsonConvert.DeserializeObject<ServiceResponse<List<CommitteeVM>>>(content);
+            committeList = user.data;
+
+            var committee = committeList.Where(m => m.CommitteeId == 1).FirstOrDefault();
+
+            return View(committee);
         }
-        public IActionResult WomenCellAttachmentsAdd()
-        {
-            return View();
-        }
-        public IActionResult WomenCellAttachmentsView()
-        {
-            return View();
-        }
+
         public IActionResult WomenCellMembersAdd()
         {
             return View();
---
+++ BACKEND_HTML_DOT_NET/Models/CommitteeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BACKEND_HTML_DOT_NET.Models
+{
+    public class CommitteeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Slogan { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedDateInt { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedDateInt { get; set; }
+        public int CommitteeId { get; set; }
+    }
+}
