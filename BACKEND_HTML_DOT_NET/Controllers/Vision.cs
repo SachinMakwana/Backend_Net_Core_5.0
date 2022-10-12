adc65df9f96f5721479d60621284de36284c5106
Nishika Thakur <86481737+Nishika4918@users.noreply.github.com>
2022-10-12 17:25:13 +0530

    CHANGES

--- BACKEND_HTML_DOT_NET/Controllers/Vision.cs
+++ BACKEND_HTML_DOT_NET/Controllers/Vision.cs
@@ -151,7 +151,7 @@ namespace BACKEND_HTML_DOT_NET.Controllers
                     var uri = new Uri(apiBaseUrl + "/UpdateVisionDetail");
                     StringContent content = new StringContent(JsonConvert.SerializeObject(visionDetail), Encoding.UTF8, "application/json");
 
-                    using (var response = client.PutAsync(uri, content))
+                    using (var response = client.PostAsync(uri, content))
                     {
                         response.Wait();
                         var results = response.Result;
@@ -189,7 +189,7 @@ namespace BACKEND_HTML_DOT_NET.Controllers
                     var uri = new Uri(apiBaseUrl + "/DeleteVisionDetail");
                     StringContent content = new StringContent(JsonConvert.SerializeObject(updateItem), Encoding.UTF8, "application/json");
 
-                    using (var response = client.PutAsync(uri, content))
+                    using (var response = client.PostAsync(uri, content))
                     {
                         response.Wait();
                         var results = response.Result;
