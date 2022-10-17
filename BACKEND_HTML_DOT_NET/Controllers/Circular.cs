3e35b2cf0b78a03faef0dd07724f626993dd8013
krish-ktm <58637118+krish-ktm@users.noreply.github.com>
2022-10-18 00:29:28 +0530

    init circular

---
+++ BACKEND_HTML_DOT_NET/Controllers/Circular.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND_HTML_DOT_NET.Controllers
+{
+    public class Circular : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
